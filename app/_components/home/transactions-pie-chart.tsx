af659b575d8318e9338c805b20ea2e150637f8ac
Gabriel Rosa <control.brosa07@gmail.com>
2024-12-23 18:36:11 -0300

    [FIX] change pie-chart

--- app/_components/home/transactions-pie-chart.tsx
+++ app/_components/home/transactions-pie-chart.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import { Pie, PieChart } from "recharts";
 
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/app/_components/ui/card";
@@ -75,10 +72,7 @@ export default function TransactionsPieChart() {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut</CardTitle>
-        <CardDescription>
-          January - June 2024
-        </CardDescription>
+        <CardTitle>Gráfico de Gastos</CardTitle>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
@@ -99,15 +93,6 @@ export default function TransactionsPieChart() {
           </PieChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month{" "}
-          <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
     </Card>
   );
 }
